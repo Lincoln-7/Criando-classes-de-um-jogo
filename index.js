36c4f93d7f4a727c16aeae13d8a0e4ed77d280e7
Lincoln-7 <ericklles62@gmail.com>
2023-12-19 21:51:14 -0300

    segundo envio

---
+++ index.js
@@ -0,0 +1,43 @@
+class Heroi {
+    
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    
+    atacar() {
+        
+        let ataque;
+        switch (this.tipo) {
+            case "mago":
+                ataque = "usou magia";
+                break;
+            case "guerreiro":
+                ataque = "usou espada";
+                break;
+            case "monge":
+                ataque = "usou artes marciais";
+                break;
+            case "ninja":
+                ataque = "usou shuriken";
+                break;
+            default:
+                ataque = "usou um ataque genérico";
+        }
+
+        
+        console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}`);
+    }
+}
+
+const mago = new Heroi("lincoln", 23, "mago");
+const guerreiro = new Heroi("ericklles", 21, "guerreiro");
+const monge = new Heroi("dennis", 34, "monge");
+const ninja = new Heroi("dayane", 25, "ninja");
+
+mago.atacar();
+guerreiro.atacar();
+monge.atacar();
+ninja.atacar();
